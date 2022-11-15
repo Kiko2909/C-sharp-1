5b021bf6ff5a1c4a77aea17122ae5814001a4f49
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 13:40:38 +0300

    add Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+int a = 0;
+
+while ( a < 100 || a > 999 )
+{
+Console.Write("Введите трёхзначное число, чтобы узнать его последнюю цифру: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if ( a > 99 && a < 1000)
+{
+    int last = a % 10;
+    Console.WriteLine($"Последняя цифра: { last }");
+}
+else Console.WriteLine("Некорректное значение.");
+
+}
+
