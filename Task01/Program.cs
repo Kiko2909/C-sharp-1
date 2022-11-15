5d5f4644dcec27eacecb8eba0343d46dda4469dd
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 12:49:17 +0300

    add Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine($"a = {a}, b = {b} -> да");
+}
+else
+{
+    Console.WriteLine($"a = {a}, b = {b} -> нет");
+}
