3219a1a5859fdd1f377183bb112e643f08ffddcd
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 12:35:04 +0300

    add Task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int result = a * a;
+
+Console.WriteLine($"Квадрат числа { a } равен { result } ");
