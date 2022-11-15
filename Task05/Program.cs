cf86cf724d6a1d0ed803b65e1e3d701b068bc698
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 14:22:25 +0300

    add Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+    Console.WriteLine($"Max = {a}");
+}
+
+else if (a == b)
+{
+    Console.WriteLine("Числа равны.");
+}
+else 
+{
+    Console.WriteLine($"Max = {b}");
+}
