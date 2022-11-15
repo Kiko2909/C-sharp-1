f6f464254ed4a68076b37e1c649139e9a29499c1
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 14:59:37 +0300

    add Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число, чтобы увидеть все чётные числа от 1 до N");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int j = 0; j <= a; j++ )
+{
+    if (j % 2 == 0 && j != 0) 
+    {
+        Console.Write($"{j} ");
+    }
+}
