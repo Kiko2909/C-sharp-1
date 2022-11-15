1c50e4ab76aa1c2876b8b703d4fb0c7b462233b8
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 14:36:30 +0300

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (max < b) 
+{
+    max = b;
+        if (max < c)
+        {
+        max = c;
+        }
+}
+else if (max < c) 
+{
+    max = c;
+}
+Console.WriteLine($"Max = {max}");
