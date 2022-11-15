e67c1f4cca955d420ed176b0fcb81803a33a1242
Olesya <olesya_usoltseva@bk.ru>
2022-11-15 14:11:36 +0300

    add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 0)
+{
+for (int b = a; b <= (a * -1) ; b++)
+{
+   Console.Write($"{b} ");
+}
+}
+
+else 
+{
+for (int b = (a * -1); b <= a ; b++)
+{
+   Console.Write($"{b} ");
+}
+}
