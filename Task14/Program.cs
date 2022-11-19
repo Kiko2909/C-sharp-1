a87f0caa8ff2cc7565f8e06b3a7af7fa5f56ffb0
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 17:59:57 +0300

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+int a = 0;
+
+while ( a < 100 || a > 999 )
+{
+Console.Write("Введите трёхзначное число, чтобы узнать его вторую цифру: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a < 100 || a > 999 ? "Некорректное значение." : "");
+}
+
+int Number(int number)
+{
+    return number / 10 % 10;
+}
+int second = Number(a);
+Console.WriteLine($"Вторая цифра: { second }");
