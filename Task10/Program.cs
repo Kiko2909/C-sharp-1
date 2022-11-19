cfc0b9c1229d8f13f1ca9750edbebc23499e40ba
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 14:47:03 +0300

    add Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа выводит случайное трёхзначное число и удаляет вторую цифру этого числа
+//897->87
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000); //[x,y)
+
+Console.WriteLine($"Случайное трёхзначное число --> {number}");
+
+int Res (int num) // int num = number
+{
+int firstDigit = num / 100;
+int thirdDigit = num % 10;
+
+return firstDigit * 10 + thirdDigit;
+}
+
+int result = Res(number);
+Console.WriteLine($"Итог --> {result}");
