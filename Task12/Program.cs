16ff23e7e6ae6c4f00e3d3087d79f5278f6cbec2
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 16:44:21 +0300

    add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Принимаем на вход число, проверяем красно ли оно одновременно и 7 и 23
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+bool Multi(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+bool result = Multi(a);
+Console.WriteLine (result ? $"Число {a} кратно 7 и 23" : "Не кратно 7 и 23");
