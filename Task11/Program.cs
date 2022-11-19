88f6d0ee81c5d95a28b00a04ef02397404d115c6
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 15:41:03 +0300

    add smth Task11

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -8,8 +8,9 @@ int b = Convert.ToInt32(Console.ReadLine());
 /*
 bool Multiplycity(int num1 , int num2)
 {
-    if (num1 % num2 == 0) return true;
-    return false;
+    return (num1 % num2 == 0);
+    //if (num1 % num2 == 0) return true;
+    //return false;
 }
 
 bool result = Multiplycity(a, b);
