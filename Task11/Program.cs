b76faada1f8a631a6df1d81257542d2938b5f45f
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 15:34:58 +0300

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+/*
+bool Multiplycity(int num1 , int num2)
+{
+    if (num1 % num2 == 0) return true;
+    return false;
+}
+
+bool result = Multiplycity(a, b);
+Console.WriteLine(result ? "Кратно" : $"Не кратно, остаток --> {a % b}");
+*/
+
+int Multi(int num1, int num2)
+{
+    return num1 % num2;
+}
+
+int result = Multi(a , b);
+Console.WriteLine(result == 0 ? "Кратно" : $"Не кратно, остаток --> {result}");
