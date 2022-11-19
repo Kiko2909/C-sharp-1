de6c3f3b763163288aede10f565a8748d12b868f
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 17:09:56 +0300

    add new method

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -5,10 +5,12 @@ Console.Clear();
 Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine(Multi(a) ? "Да" : "Нет");
+
 bool Multi(int number)
 {
     return number % 7 == 0 && number % 23 == 0;
 }
 
-bool result = Multi(a);
-Console.WriteLine (result ? $"Число {a} кратно 7 и 23" : "Не кратно 7 и 23");
+// bool result = Multi(a);
+// Console.WriteLine (result ? $"Число {a} кратно 7 и 23" : "Не кратно 7 и 23");
