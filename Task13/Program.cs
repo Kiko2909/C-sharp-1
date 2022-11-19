b29c102e36e762925d67b0ae41ca0ca95837d97a
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-19 17:08:19 +0300

    add Task13 + new method Task12

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает два числа и проверяет, является ли одно число квадратом другого
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Square(a, b) ? "Да" : "Нет");
+
+bool Square (int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+// bool result = Square(a, b);
+// Console.WriteLine(result ? "Да" : "Нет" );
