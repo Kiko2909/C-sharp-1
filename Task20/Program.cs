ba7836dbcad429265b1f6541f2a1fd7ece917de9
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-21 18:14:20 +0300

    add Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,18 @@
+﻿// принимает на вход число N и выдаёт таблицу квадратов чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Table (int n)
+{        
+    int x = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{x} --> {x*x}"); 
+        x+=1;
+    }
+    return 0;
+}
+Table(num);
