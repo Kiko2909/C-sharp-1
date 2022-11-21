c519dfea48239855c9d87a5f9a2b4df7af478e6b
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-21 14:01:12 +0300

    add Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,18 @@
+﻿// по заданному номеру четверти показывает диапазон данных
+Console.Clear();
+
+Console.WriteLine("Введите номер четверти: ");
+string x = Console.ReadLine();
+
+
+string Quarter(string xc)
+{
+    if (xc == "1" ) return "x > 0 и y > 0";
+    if (xc == "2" ) return "x < 0 и y > 0";
+    if (xc == "3" ) return "x < 0 и y < 0";
+    if (xc == "4" ) return "x > 0 и y < 0";
+    return "Введены некорректные данные.";
+}
+
+string result = Quarter(x);
+Console.WriteLine($"{result}");
