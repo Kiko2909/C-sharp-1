cf54f54b922e74e60050c2a57fe626b98b8e3710
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-21 13:08:28 +0300

    add dop

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -6,6 +6,9 @@ while ( a < 100 || a > 999 )
 {
 Console.Write("Введите трёхзначное число, чтобы узнать его вторую цифру: ");
 a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 0) a = a * -1;
+
 Console.WriteLine(a < 100 || a > 999 ? "Некорректное значение." : "");
 }
 
