6dfbf1b424d18ef7d934bc4fb4ad06407e566ed5
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-21 13:38:22 +0300

    add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,23 @@
+﻿// принимает координаты х и у которые не равны 0 и выдаёт номер четверти плоскости в которой находится точка
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты точек: ");
+Console.Write("X = ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y = ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int Quarter(int xc, int yc)
+{
+    if (xc > 0 && yc > 0 ) return 1;
+    if (xc < 0 && yc > 0 ) return 2;
+    if (xc < 0 && yc < 0 ) return 3;
+    if (xc > 0 && yc < 0 ) return 4;
+    return 0;
+}
+
+int quarter = Quarter(x, y);
+string result = quarter > 0 ? $"Указанные координаты соответсвуют {quarter} четверти." 
+: $"Введены некорректные координаты. ";
+Console.WriteLine($"{result}");
