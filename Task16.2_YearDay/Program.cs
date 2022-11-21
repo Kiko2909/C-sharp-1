a5579dac1922285ad4e94097e0a964d225af6ce7
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-21 12:26:56 +0300

    test task

---
+++ Task16.2_YearDay/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает на вход число и показвает дату и день недели 2023 года
+
+Console.Clear();
+
+int a =0;
+
+while (a < 1 || a > 365)
+{
+    Console.WriteLine("Введите число, чтобы узнать день 2023 года: ");
+    a = Convert.ToInt32(Console.ReadLine());
+
+    if (a < 0 || a < 366)
+        {
+                
+        }
+        
+    else Console.WriteLine("Такого дня нет в 2023 году.");
+}
