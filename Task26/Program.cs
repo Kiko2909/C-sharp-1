3acb1e4de8b574a111e4a6e6d07fba6d0a8f2037
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-22 12:01:28 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿// принимает число и выдаёт произведение
+
+Console.Clear();
+
+int a = 0;
+
+while (a < 1)
+{
+Console.WriteLine("Введите положительное число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a < 0) Console.WriteLine("Введено некорретное число.");
+}
+
+int Sum (int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= num; i++) sum = sum * i;
+    return sum;
+}
+int result = Sum(a);
+Console.WriteLine($"{a} --> {result}");
