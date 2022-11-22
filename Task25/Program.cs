ec49e9d96241a643eb0a011739bba98c15bf18fd
Kiko2909 <olesya_usoltseva@bk.ru>
2022-11-22 11:18:08 +0300

    add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход число и выдаёт кол-во цифр в числе
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 0) a *= -1;
+
+int Count(int number)
+{
+    int length = 0;
+    while(number > 0)
+    {
+        number = number / 10;
+        length = length + 1;
+    }
+    return length;
+}
+int result = Count (a);
+Console.WriteLine($"{result}");
