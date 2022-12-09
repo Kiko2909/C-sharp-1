5da3c0cee2bb9ea5af9fd84695370c9973550a96
Kiko2909 <olesya_usoltseva@bk.ru>
2022-12-09 22:45:51 +0300

    add Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,54 @@
+﻿// преобразовывает десятичное число в двоичное
+//45 - 101101
+// 3 - 11
+// 2 - 10
+
+Console.Clear();
+
+/*
+string Binary(int numb)
+{
+    string bin = string.Empty;
+    while (numb > 0)
+    {
+        bin = numb % 2 + bin;
+        numb /=2;
+    }
+    return bin;
+}
+*/
+
+int[] Binary (int numb)
+{
+    int numb1 = numb;
+    int digits = 0;
+
+    while (numb1 > 0)
+    {
+        numb1 /= 2;
+        digits ++;
+    }
+
+    int[] bin = new int [digits];
+
+    for (int i = 0; i < bin.Length; i++)
+    {
+        bin [bin.Length - i - 1] = numb % 2;
+        numb /= 2;
+    }
+    return bin;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] binary = Binary(a);
+PrintArray(binary);
+Console.WriteLine();
