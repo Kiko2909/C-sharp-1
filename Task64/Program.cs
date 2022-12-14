312ec9e3fb950134dd6947de1424410f19d014c3
Kiko2909 <olesya_usoltseva@bk.ru>
2022-12-15 00:28:04 +0300

    add Task65

---
+++ Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
+
+int Sum (int num)
+{
+    return num != 0 ? num % 10 + Sum(num / 10) : 0;
+}
+Console.WriteLine($"Сумма чисел --> {Sum(number)}");
